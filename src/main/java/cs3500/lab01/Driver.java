0895701d7c2c1b38e2b229260400fa8df620adf4
Mark Fontenot <m.fontenot@northeastern.edu>
2023-04-21 05:44:44 -0400

    Update to the main driver.

--- src/main/java/cs3500/lab01/Driver.java
+++ src/main/java/cs3500/lab01/Driver.java
@@ -8,6 +8,6 @@ public class Driver {
     public static void main(String[] args) {
         Student s = new Student("Jane Doe", 3.7);
         System.out.println("Students name is " + s);
-        System.out.println("Hello world!");
+        System.out.println("Thanks for using our program!!");
     }
 }
